cac1d7717cef21c32249452b0d89e11bc4242d5f
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2022-11-29 18:54:53 -0300

    initial commit

---
+++ LabShoppingList/src/main/java/futuroDev/Projeto03/LabShoppingList/LabShoppingListApplication.java
@@ -0,0 +1,13 @@
+package futuroDev.Projeto03.LabShoppingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabShoppingListApplication.class, args);
+	}
+
+}
---
+++ LabShoppingList/src/test/java/futuroDev/Projeto03/LabShoppingList/LabShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package futuroDev.Projeto03.LabShoppingList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
