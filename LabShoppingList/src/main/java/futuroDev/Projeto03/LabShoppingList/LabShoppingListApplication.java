bc4ded311675f226b97205e2e42c5d45b74a1f7d
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2022-11-30 14:28:56 -0300

    conexão com postgres

--- LabShoppingList/src/main/java/futuroDev/Projeto03/LabShoppingList/LabShoppingListApplication.java
+++ LabShoppingList/src/main/java/futuroDev/Projeto03/LabShoppingList/LabShoppingListApplication.java
@@ -2,7 +2,10 @@ package futuroDev.Projeto03.LabShoppingList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class LabShoppingListApplication {
 
@@ -10,4 +13,10 @@ public class LabShoppingListApplication {
 		SpringApplication.run(LabShoppingListApplication.class, args);
 	}
 
+//	@GetMapping("/teste")
+//	public String teste() {
+//		return "Teste";
+//	}
 }
+
+
---
+++ LabShoppingList/src/main/java/futuroDev/Projeto03/LabShoppingList/models/Produto.java
@@ -0,0 +1,53 @@
+package futuroDev.Projeto03.LabShoppingList.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name="produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String descricao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id == produto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
